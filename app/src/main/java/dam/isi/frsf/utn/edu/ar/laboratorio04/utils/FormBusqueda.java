8ded2ef1da70415ff93561a527f5323d5d167afa
Gabriel Gerlero <gerlero@users.noreply.github.com>
2016-09-27 01:53:49 -0300

    Add method to test if a given apartment matches the search criteria

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/utils/FormBusqueda.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/utils/FormBusqueda.java
@@ -3,11 +3,16 @@ package dam.isi.frsf.utn.edu.ar.laboratorio04.utils;
 import java.io.Serializable;
 
 import dam.isi.frsf.utn.edu.ar.laboratorio04.modelo.Ciudad;
+import dam.isi.frsf.utn.edu.ar.laboratorio04.modelo.Departamento;
 
 /**
  * Created by martdominguez on 22/09/2016.
  */
 public class FormBusqueda implements Serializable {
+    //Note: all fields are nullable.
+    //A field being null means that that specific criterion is unspecified for the search (i.e., the
+    //user has no particular preference).
+    //We cannot use java.util.Optional to express this due to compatibility requirements.
     private Double precioMinimo;
     private Double precioMaximo;
     private Ciudad ciudad;
@@ -70,5 +75,18 @@ public class FormBusqueda implements Serializable {
 
     public void setHuespedes(Integer huespedes) {
         this.huespedes = huespedes;
+    }
+
+    public boolean matches(Departamento apartment) {
+        if(apartment == null) throw new NullPointerException("apartment must not be null");
+
+        //Test apartment against all criteria
+        if(precioMinimo != null && apartment.getPrecio() < precioMinimo) return false;
+        if(precioMaximo != null && apartment.getPrecio() > precioMaximo) return false;
+        if(ciudad != null && !apartment.getCiudad().equals(ciudad)) return false;
+        if(permiteFumar != null && apartment.getNoFumador() == permiteFumar) return false;
+        if(huespedes != null && apartment.getCapacidadMaxima() > huespedes) return false;
+
+        return true;
     }
 }
